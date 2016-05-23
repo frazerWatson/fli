dd789983cb642a26d7ed9d7ed9c517a383e08112
Frazer Watson <frazerw@tutanota.com>
2016-05-23 09:56:04 +0100

    Added wil_paginate gem.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # resources :contacts, only: [:new, :create]
 
   root "posts#index"
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 end
